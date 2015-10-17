c072b45c88cdbe7138d72ed261f5f5d0e58e1d3b
Ubuntu <azureuser@houdini-mongo.houdini-mongo.g7.internal.cloudapp.net>
2015-10-17 14:49:34 +0000

    Modified sample secrets file

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -1,9 +1,12 @@
 module.exports = {
 
-  db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://localhost:27017/houdini'
+  db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://localhost:27017/houdini',
 
-  //sessionSecret: process.env.SESSION_SECRET || 'f29b78a386a8a0b9fb2868c4fcf95a41343a0175865c591cbca910a48524dde9',
+  //sessionSecret: process.env.SESSION_SECRET || 'CFH4IzHAfn4MziteME7kO4Slavq31o9r1Q647Apripk44e9TZNxoWpX38xUv1Q0N',
 
   // bcrypt password seed
-  //passwordSeed: '$2a$10$gHwYF/h8hADr5p1PPQPNfO'
+  //passwordSeed: '$2a$10$MEoqtT2UfEKFrLPeudvJ1e'
+
+  stripeApiKey: 'sk_test_shvGMQoIXVZRaVqioBSqDpDI',
+  stripeClientKey: 'ca_7BDuGtRIPRxUrUqYSo3fx6DXmV9aVwsA'
 };
--- server/config/secrets.sample.js
+++ server/config/secrets.sample.js
@@ -2,10 +2,10 @@ module.exports = {
 
   db: process.env.MONGOLAB_URI || process.env.MONGODB || 'mongodb://localhost:27017/houdini',
 
-  //sessionSecret: process.env.SESSION_SECRET || 'f29b78a386a8a0b9fb2868c4fcf95a41343a0175865c591cbca910a48524dde9',
+  //sessionSecret: process.env.SESSION_SECRET || 'mySeed',
 
   // bcrypt password seed
-  //passwordSeed: '$2a$10$gHwYF/h8hADr5p1PPQPNfO'
+  //passwordSeed: 'mySeed'
 
   stripeApiKey: 'api_key',
   stripeClientKey: 'client_key'
