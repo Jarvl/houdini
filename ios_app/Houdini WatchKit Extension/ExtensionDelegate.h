73cad9aa6ae74d7eb4663fe86921ff90076d1504
Luis Finke <luisfinke@gmail.com>
2015-10-17 04:23:36 -0500

    Added iOS app code

---
+++ ios_app/Houdini WatchKit Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  Houdini WatchKit Extension
+//
+//  Created by Apple on 10/16/15.
+//  Copyright © 2015 lufinkey. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ ios_app/Houdini/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Houdini
+//
+//  Created by Apple on 10/16/15.
+//  Copyright © 2015 lufinkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
