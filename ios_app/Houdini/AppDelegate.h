3585b16c3b7cce06d5a39151837d21b6813a7a64
Luis Finke <luisfinke@gmail.com>
2015-10-18 11:04:29 -0400

    jesus fucking christ

--- ios_app/Houdini/AppDelegate.h
+++ ios_app/Houdini/AppDelegate.h
@@ -1,7 +1,8 @@
 
 #import <UIKit/UIKit.h>
+#import "CallMonitor.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UIAlertViewDelegate, CallMonitorDelegate>
 @property (strong, nonatomic) UIWindow *window;
 @end
 
